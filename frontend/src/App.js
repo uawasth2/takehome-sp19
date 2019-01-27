8c5aee44443d2a1ee0e75f30c7625b0ee1f98742
Navam <=>
2019-01-27 00:52:38 -0600

    p1 frontend done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <Instructions />
+        <Instructions complete={true}/>
         {this.state.shows.map(x => (
           <Show id={x.id} name={x.name} episodes_seen={x.episodes_seen} />
         ))}
